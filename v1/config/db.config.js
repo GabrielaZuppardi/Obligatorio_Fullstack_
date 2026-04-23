385d5c97acb9ff11583fc8451a29173607931edf
gnzm15 <gnzm.15@gmail.com>
2026-04-22 21:40:06 -0300

    avances

--- v1/config/db.config.js
+++ v1/config/db.config.js
@@ -1,14 +1,51 @@
-import mongoose from "mongoose";
+/*import mongoose from "mongoose";
 
 // Database configuration
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI_ATLAS);
     console.log("Conectado a MongoDB");
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
     process.exit(1); // Detiene la ejecución del servidor si no se puede conectar a la base de datos
+  } 
+};
+
+export default connectDB;*/
+
+import mongoose from "mongoose";
+/*
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI_ATLAS || process.env.MONGO_URI;
+
+    console.log("Conectando a:", uri);
+
+    await mongoose.connect(uri);
+
+    if (uri.includes("mongodb+srv") || uri.includes("mongodb://gz331760_db_user")) {
+      console.log("Conectado a MongoDB Atlas ☁️");
+    } else {
+      console.log("Conectado a MongoDB Local 💻");
+    }
+
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  } 
+};*/
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI_ATLAS || process.env.MONGO_URI; //primero intenta conectar a Atlas, si no está disponible, conecta a local
+    console.log("Conectando a:", uri);
+    await mongoose.connect(uri);
+
+    console.log("Conectado a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
   } 
 };
 
