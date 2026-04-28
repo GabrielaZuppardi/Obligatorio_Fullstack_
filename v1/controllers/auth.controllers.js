01b35b7662ee1887ff38fb1a9d7414a6c8a4505e
gnzm15 <gnzm.15@gmail.com>
2026-04-27 23:37:29 -0300

    agregu{e try catch

--- v1/controllers/auth.controllers.js
+++ v1/controllers/auth.controllers.js
@@ -1,16 +1,19 @@
 import {registrarUsuarioService, loginUsuarioService} from "../services/auth.services.js";
 
 export const registrarUsuario = async (req, res, next) => {
-   
-        const { usuario, token } = await registrarUsuarioService(req.body);
+  try {
+    const { usuario, token } = await registrarUsuarioService(req.body);
 
-        res.status(201).json({
-            mensaje: "Usuario registrado correctamente",
-            usuario,
-            token
-        });
+    res.status(201).json({
+      mensaje: "Usuario registrado correctamente",
+      usuario,
+      token
+    });
 
-    };
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const loginUsuario = async (req, res, next) => {
 
