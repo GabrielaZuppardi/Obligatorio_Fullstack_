6c1b7bdd5963bbb86a98d7e730130f1080af056e
gnzm15 <gnzm.15@gmail.com>
2026-04-27 23:22:33 -0300

    intento de arreglar test

--- v1/routes/auth.routes.js
+++ v1/routes/auth.routes.js
@@ -3,10 +3,13 @@ import {
     registrarUsuario,
     loginUsuario
 } from "../controllers/auth.controllers.js";
+import { validateBody } from "../middlewares/validateBody.middleware.js";
+import { crearUsuarioSchema, modificarUsuarioSchema } from "../validators/usuario.validator.js";
+
 
 const router = express.Router({ mergeParams: true });
 
-router.post("/registro", registrarUsuario);
+router.post("/registro", validateBody(crearUsuarioSchema), registrarUsuario);
 router.post("/login", loginUsuario);
 
 export default router;
